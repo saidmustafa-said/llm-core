1e79aeea7799556ee9bb886689e36be4b4da033d
Said Mustafa Said <50619400+Said7Mustafa@users.noreply.github.com>
2025-02-28 13:20:43 +0300

    Initial commit on get_started branch

--- fine-tuining/a.py
+++
@@ -1,7 +0,0 @@
-import torch
-print(torch.__version__)
-print(torch.version.cuda)
-print(torch.backends.cudnn.version())
-print(torch.cuda.is_available())
-print(torch.cuda.device_count())
-print(torch.cuda.get_device_name(0))
